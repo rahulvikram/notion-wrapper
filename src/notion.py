0ff3a04fd9f0096f0faf79e217b334f0397824e3
Rahul Vikram <rahulsvikram99@gmail.com>
2024-09-01 19:49:12 -0700

    src files

---
+++ src/notion.py
@@ -0,0 +1,8 @@
+import notion
+
+class Notion:
+    def __init__(self, auth_details):
+        # must pass auth details such as key, user, database, or whatever
+        # figure this out later
+        pass
+    
